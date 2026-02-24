ebf39bbae9a1009383c66bbcd2f993e7b19a12b7
{Rahul B} <{rahulbisht1301@gmail.com}>
2026-02-24 23:42:36 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rahul Bisht",
+    author_email="rahulbisht1301@gmail.com",
+    packages=find_packages()
+)
