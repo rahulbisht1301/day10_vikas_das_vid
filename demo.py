8f98c0343ed4e1a380a7c1471c0347d31cbfaeda
{Rahul B} <{rahulbisht1301@gmail.com}>
2026-02-27 18:49:29 +0530

    1:-ran the demo.py  2:- confirmed the data ingestion is working fine by bring data from mongo db then preprocess it 3:-After data ingestion the storing of data inside artifact directory is working fine

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 # # below code is to check the logging config
-from src.logger import logging
+# from src.logger import logging
 
 # logging.debug("This is a debug message.")
 # logging.info("This is an info message.")
@@ -9,20 +9,20 @@ from src.logger import logging
 
 # # --------------------------------------------------------------------------------
 
-# # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# # # # below code is to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
 
 # # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
